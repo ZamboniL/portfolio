f29b6f3e6aa7f136378e37ddbabb8479d7f3f600
ZamboniL <lucaszam0@gmail.com>
2026-08-23 11:09:45 -0300

    fix rss generation

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,20 @@
 import { getCollection } from "astro:content";
+import { getRelativeLocaleUrl } from "astro:i18n";
 import rss from "@astrojs/rss";
 import { useTranslations } from "#i18n/helper.ts";
 
+
 export async function GET(context) {
-  const posts = await getCollection("blog");
-  const t = useTranslations(Astro.currentLocale, "head");
+  const posts = await getCollection("writing");
+  const t = useTranslations(context.currentLocale, "head");
   return rss({
     description: t("description"),
-    items: posts.map((post) => {
-      const item = Object.assign(post.data, { link: `/blog/${post.id}/` });
-      return item;
-    }),
+    items: posts.map((post) => ({
+      description: post.data.description,
+      link: getRelativeLocaleUrl(context.currentLocale, `/writing/${post.id}/`),
+      pubDate: post.data.pubDate,
+      title: post.data.title,
+    })),
     site: context.site,
     title: t("title"),
   });
