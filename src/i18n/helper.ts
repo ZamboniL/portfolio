2a8e36cc3ec2bc03b97daef4353658d1a9176805
ZamboniL <lucaszam0@gmail.com>
2026-08-23 10:27:34 -0300

    hook translations into pages

--- src/i18n/helper.ts
+++ src/i18n/helper.ts
@@ -1,32 +1,58 @@
-import type { LOCALES } from "./consts";
+import { DEFAULT_LOCALE, LOCALES } from "./consts";
 
 const translations = {
   en: {
     footer: {
+      email: "Send me an email",
+      github: "Go to my GitHub profile",
+      linkedin: "Go to my LinkedIn profile",
       title: "Software Engineer",
     },
     head: {
       description: "A blog about web development, programming and technology.",
       title: "Lucas Zamboni",
     },
+    home: {
+      intro: `Hi, I'm Lucas, a software engineer that is starting to blog about web
+        development, programming, and technology.`,
+      "writing.all": "View all",
+      "writing.title": "My writing",
+    },
     nav: {
       about: "about",
       locale: "EN",
       writing: "writing",
     },
+    writing: {
+      description: "Things I did for work and fun",
+      title: "My writing",
+    },
   },
   "pt-br": {
     footer: {
+      email: "Me envie um e-mail",
+      github: "Visite meu Github",
+      linkedin: "Visite meu LinkedIn",
       title: "Engenheiro de Software",
     },
     head: {
       description: "um blog sobre desenvolvimento web, programação e tecnologia.",
       title: "Lucas Zamboni",
     },
+    home: {
+      intro: `Olá, eu sou o Lucas, um engenheiro de software que está começando
+        a blogar sobre desenvolvimento web, programação e tecnologia.`,
+      "writing.all": "Ver todos",
+      "writing.title": "Minha escrita",
+    },
     nav: {
       about: "sobre",
       locale: "PTBR",
       writing: "escrita",
+    },
+    writing: {
+      description: "O que eu fiz para trabalho e diversão",
+      title: "Minha escrita",
     },
   },
 } as const;
@@ -35,6 +61,11 @@ type Translations = typeof translations;
 type Locales = (typeof LOCALES)[number];
 type Sections = keyof Translations[Locales];
 
-export function useTranslations<T extends Sections>(locale: Locales, section: T) {
-  return (t: keyof Translations[Locales][T]) => translations[locale][section][t];
+export function useTranslations<T extends Sections>(locale: string | undefined, section: T) {
+  const validLocale = (locale as Locales) ?? DEFAULT_LOCALE;
+  if (!LOCALES.includes(validLocale)) {
+    throw new Error("Invalid locale");
+  }
+
+  return (t: keyof Translations[Locales][T]) => translations[validLocale][section][t];
 }
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,17 +1,17 @@
-import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-
-import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
+import rss from "@astrojs/rss";
+import { useTranslations } from "../i18n/helper";
 
 export async function GET(context) {
   const posts = await getCollection("blog");
+  const t = useTranslations(Astro.currentLocale, "head");
   return rss({
-    description: SITE_DESCRIPTION,
+    description: t("description"),
     items: posts.map((post) => {
       const item = Object.assign(post.data, { link: `/blog/${post.id}/` });
       return item;
     }),
     site: context.site,
-    title: SITE_TITLE,
+    title: t("title"),
   });
 }
